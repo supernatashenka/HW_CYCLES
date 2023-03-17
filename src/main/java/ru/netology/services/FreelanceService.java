3a044bb966f633330e8940ebd73c6480d6655f66
Netology QA <obgynkostina@gmail.com>
2023-03-17 19:19:31 +0300

    modified test

--- src/main/java/ru/netology/services/FreelanceService.java
+++ src/main/java/ru/netology/services/FreelanceService.java
@@ -4,9 +4,9 @@ public class FreelanceService {
     public int calculate(int income, int expense, int threshold) {
         int money = 0;
         int counter = 0;
-        for (int month = 1; month <=12; month++) {
+        for (int month = 1; month <= 12; month++) {
             if (money >= threshold) {
-                money = (money - expense)/3;
+                money = (money - expense) / 3;
                 counter++;
             } else {
                 money = money + income - expense;
--- src/test/java/ru/netology/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -7,10 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FreelanceServiceTest {
     @Test
-    public void income10k (){
+    public void income10k() {
         FreelanceService service = new FreelanceService();
-        int actual =  service.calculate(10_000, 3_000, 20_000);
-        int expected = 3 ;
-        Assertions.assertEquals(expected, actual );
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+        Assertions.assertEquals(expected, actual);
+    }
+
+        @Test
+        public void income100k() {
+            FreelanceService service = new FreelanceService();
+            int actual = service.calculate(100_000, 60_000, 150_000);
+            int expected = 2;
+            Assertions.assertEquals(expected, actual);
     }
 }
